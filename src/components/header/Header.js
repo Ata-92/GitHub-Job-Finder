3af75115b517e0257d61b49e7ccb411b3e13992a
Ata-92 <atakal92@gmail.com>
2021-05-15 19:16:16 +0300

    Complete Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,14 @@
 import octocat from "../../assets/octocat.png";
-import job-logo from "../../assets/job-logo.svg";
+import joblogo from "../../assets/job-logo.svg";
 
 const Header = () => {
   return (
     <div className="Header">
-
+      <div className="logos">
+        <img src={octocat} alt="GitHub Logo" className="github-logo"/>
+        <img src={joblogo} alt="Job Logo" className="job-logo"/>
+      </div>
+      <h1>GITHUB JOB FINDER</h1>
     </div>
   )
 }
