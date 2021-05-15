c408bec017d3d740006790041bc14cc789f72edb
Ata-92 <atakal92@gmail.com>
2021-05-15 22:52:16 +0300

    Add a wrapper and complete Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,6 +4,13 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="Footer">
+      <div className="wrapper">
+        <span className="wrapper__designer">
+          <em>{String(`<ata/>`)}</em>
+        </span>
+        <img src={design} alt="Design Icon" className="wrapper__icon" />
+        <span className="wrapper__design">design</span>
+      </div>
     </div>
   );
 };
