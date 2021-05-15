cc59c4c87b0b6996dc49bcb531f0431695ab053c
Ata-92 <atakal92@gmail.com>
2021-05-15 22:51:33 +0300

    Import an image and create function component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Form from "./components/form/Form";
 
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import design from "../../assets/design.svg";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+    </div>
+  );
+};
+
+export default Footer;
