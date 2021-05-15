070ae50725aaec6f54c3568c0aa431a487adc4bc
Ata-92 <atakal92@gmail.com>
2021-05-15 19:12:38 +0300

    Change title and import 2 images

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import octocat from "../../assets/octocat.png";
+import job-logo from "../../assets/job-logo.svg";
+
+const Header = () => {
+  return (
+    <div className="Header">
+
+    </div>
+  )
+}
+
+export default Header;
