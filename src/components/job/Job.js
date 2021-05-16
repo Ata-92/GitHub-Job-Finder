771fe6dea2549849807c320724a10c1030cbed4f
Ata-92 <atakal92@gmail.com>
2021-05-16 17:57:26 +0300

    Add bootstrap card class to Job comp.

--- src/components/job/Job.js
+++ src/components/job/Job.js
@@ -4,12 +4,14 @@ const Job = ({data}) => {
 
 
   return (
-    <div className="Job">
-      <img src={data.company_logo} alt="Company Logo" className="company-logo"/>
-      <p className="title">{data.title}</p>
-      <p className="company-and-location">{data.company_and_location}</p>
-      <p className="job-type">{data.type}</p>
-      <button className="job-apply"><a href={data.apply}>APPLY</a></button>
+    <div className="Job card rounded-5 w-50">
+      <img src={data.company_logo} alt="Company Logo" className="company-logo card-img-top w-25"/>
+      <h2 className="title card-header bg-secondary text-warning">{data.title}</h2>
+      <div className="card-body">
+        <p className="company-and-location">{data.company_and_location}</p>
+        <p className="job-type">{data.type}</p>
+        <button className="job-apply btn btn-success text-white"><a href={data.apply}>APPLY</a></button>
+      </div>
     </div>
   )
 }
