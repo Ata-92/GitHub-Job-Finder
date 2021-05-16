552fb9e9fe701aea14cd84c70cfceb37898fcfaf
Ata-92 <atakal92@gmail.com>
2021-05-16 13:28:05 +0300

    Create a job card

--- src/components/job/Job.js
+++ src/components/job/Job.js
@@ -5,6 +5,11 @@ const Job = ({data}) => {
 
   return (
     <div className="Job">
+      <img src={data.company_logo} alt="Company Logo" className="company-logo"/>
+      <p className="title">{data.title}</p>
+      <p className="company-and-location">{data.company_and_location}</p>
+      <p className="job-type">{data.type}</p>
+      <button className="job-apply"><a href={data.apply}>APPLY</a></button>
     </div>
   )
 }
