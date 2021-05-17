a27678018938a60f7afe4305bbb819ce91cc6393
Ata-92 <atakal92@gmail.com>
2021-05-17 07:56:23 +0300

    Style Job component

--- src/components/job/Job.js
+++ src/components/job/Job.js
@@ -1,19 +1,34 @@
 import "./Job.css";
 
-const Job = ({data}) => {
-
-
+const Job = ({ data }) => {
   return (
-    <div className="Job card rounded-5 w-50">
-      <img src={data.company_logo} alt="Company Logo" className="company-logo card-img-top w-25"/>
-      <h2 className="title card-header bg-secondary text-warning">{data.title}</h2>
-      <div className="card-body">
-        <p className="company-and-location">{data.company_and_location}</p>
-        <p className="job-type">{data.type}</p>
-        <button className="job-apply btn btn-success text-white"><a href={data.apply}>APPLY</a></button>
+    <div className="Job card border border-success rounded w-50 my-5 text-uppercase">
+      <img
+        src={data.company_logo}
+        alt="Company Logo"
+        className="company-logo card-img-top w-25"
+      />
+      <div className="job-info border border-success">
+        <h2 className="title card-header text-danger">
+          {data.title.split(" ").map((word, index) => (
+            <span key={`word${index}`}><span className="initial">{word[0]}</span>{word.slice(1)} </span>
+          ))}
+        </h2>
+        <div className="card-body">
+          <p className="company-and-location fw-bold">
+            {data.company_and_location}
+          </p>
+          <p className="job-type">{data.type}</p>
+          <a
+            className="job-apply btn btn-success text-white w-50"
+            href={data.apply} rel="noreferrer" target="_blank"
+          >
+            APPLY
+          </a>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Job;
