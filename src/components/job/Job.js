434390a8a6a6f8c2c00b3beb6621e6d3f4e79743
Ata-92 <atakal92@gmail.com>
2021-05-17 08:40:47 +0300

    Adjust logos size

--- src/components/job/Job.js
+++ src/components/job/Job.js
@@ -6,19 +6,23 @@ const Job = ({ data }) => {
       <img
         src={data.company_logo}
         alt="Company Logo"
-        className="company-logo card-img-top w-25"
+        className="company-logo card-img-top py-3"
       />
       <div className="job-info border border-success">
         <h2 className="title card-header text-danger">
           {data.title.split(" ").map((word, index) => (
-            <span key={`word${index}`}><span className="initial">{word[0]}</span>{word.slice(1)} </span>
+            <span key={`word${index}`}><span className="title-initial">{word[0]}</span>{word.slice(1)} </span>
           ))}
         </h2>
         <div className="card-body">
           <p className="company-and-location fw-bold">
-            {data.company_and_location}
+            {data.company_and_location.split(" ").map((word, index) => (
+            <span key={`word${index}`}><span className="cl-initial">{word[0]}</span>{word.slice(1)} </span>
+          ))}
           </p>
-          <p className="job-type">{data.type}</p>
+          <p className="job-type">{data.type.split(" ").map((word, index) => (
+            <span key={`word${index}`}><span className="type-initial">{word[0]}</span>{word.slice(1)} </span>
+          ))}</p>
           <a
             className="job-apply btn btn-success text-white w-50"
             href={data.apply.split("href=")[1].split(">")[0].slice(1)} rel="noreferrer" target="_blank"
